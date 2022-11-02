ece4d8e971b3fd0983af597ee506ba7e2b5ddbec
prudhviraj45 <115442733+prudhviraj45@users.noreply.github.com>
2022-11-02 16:36:10 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,8 @@
+output "ec2_arn" {
+  value = aws_instance.my-machine.arn
+}
+
+output "instance_ip_addr" {
+  value       = aws_instance.my-machine.public_ip
+  description = "The public IP address of the main server instance."
+}
