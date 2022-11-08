4ecda21a0e8d55c7b8e1f972eb3f4feca1e40b82
prudhviraj45 <115442733+prudhviraj45@users.noreply.github.com>
2022-11-08 18:05:30 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,8 +1,10 @@
 terraform {
-required_providers {
-aws = {
-source = "hashicorp/aws"
-version = "~> 4.1"
+  required_version = ">= 1.3.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.20.0"
     }
   }
-} 
+}
